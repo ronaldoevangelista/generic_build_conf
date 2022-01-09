86606a417e678c4759b0328f8ab4ffe1a974b339
Ronaldo Evangelista <ronaldinho.evangelista@gmail.com>
2022-01-08 21:52:54 -0300

    add new features

--- init.rb
+++ init.rb
@@ -1,16 +1,39 @@
 require 'autoproj/gitorious'
 
-os_names, os_releases = Autoproj.workspace.operating_system
-
-Autoproj.message("os_names values: #{os_names}", :red)
-
-Autoproj.message(Autoproj.operating_system, :red)
-
-
-
-
 Autobuild.parallel_build_level = 10
 Autobuild::CMake.show_make_messages = true
-Autobuild::CMake.prefix_path << Autoproj.prefix
 
-Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
+os_names, os_releases = Autoproj.workspace.operating_system
+
+ros_distro = ''
+so_distro=''
+
+distro_map = {
+    'xenial' => 'kinetic',
+    'bionic' => 'melodic',
+    'focal'  => 'noetic'
+}
+
+ros_distro = distro_map[os_releases.find { |release| distro_map[release]}]
+
+distro_map.each do |so_name,ros_name|
+  if (ros_distro == ros_name)
+    ros_distro= ros_name
+    so_distro=so_name
+  end
+end
+
+if ros_distro == 'noetic'
+  python_path = 'python3'
+else
+  python_path = 'python2.7'
+end
+
+Autoproj.message "  ros #{ros_distro}", :blue
+Autoproj.message "  distro #{so_distro}", :red
+Autoproj.message "  #{python_path} environment", :green
+
+Autoproj.env_add_path 'PYTHONPATH', File.join("python_path", 'dist-packages')
+Autoproj.config.set('USE_PYTHON', 'YES') unless Autoproj.config.get('USE_PYTHON', nil)
+
+Autobuild::CMake.prefix_path << Autoproj.prefix
