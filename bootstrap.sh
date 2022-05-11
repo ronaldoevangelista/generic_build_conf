db3f5469533aff8b078104fddad6c18c0920ef0a
Ronaldo Evangelista <ronaldinho.evangelista@gmail.com>
2022-05-11 08:46:31 -0300

    test orise package

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,43 @@
 #!/bin/sh
+# Copyright (c) 2022
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-AUTOPROJ_INSTALL_URL=https://raw.githubusercontent.com/rock-core/autoproj/master/bin/autoproj_install
+set -ex
 
-BOOTSTRAP_URL=git@github.com:Brazilian-Institute-of-Robotics/generic_build_conf.git
+export AUTOPROJ_VERSION=""
+export BOOTSTRAP_MANIFEST=manifest
+export AUTOPROJ_OSDEPS_MODE=all
+export AUTOPROJ_BOOTSTRAP_IGNORE_NONEMPTY_DIR=1
+export PYTHON_EXECUTABLE=/usr/bin/python3
 
 PWD=$(pwd)
-
-AUTOPROJ_VERSION=""
-
 MANIFEST_FILE=${PWD}/autoproj
 
-BOOTSTRAP_BRANCH=master
+BOOTSTRAP_BRANCH=feat-add-build-files
 
-BOOTSTRAP_MANIFEST=manifest
+AUTOPROJ_INSTALL_URL=https://raw.githubusercontent.com/rock-core/autoproj/master/bin/autoproj_install
+BOOTSTRAP_URL=git@github.com:ronaldoevangelista/generic_build_conf.git
+
+[ -f "autoproj_install" ] || wget -nv ${AUTOPROJ_INSTALL_URL}
+
+[ -f "autoproj.gemfile" ] || { cat <<EOF > autoproj.gemfile; }
+source "https://rubygems.org"
+gem "autoproj", github: 'rock-core/autoproj'
+gem "autobuild", github: 'rock-core/autobuild'
+EOF
+
+
 
 set -e
 [ -f "autoproj_install" ] || wget -nv ${AUTOPROJ_INSTALL_URL}
@@ -20,23 +45,30 @@ set -e
   mkdir -p .autoproj
   cat <<EOF >.autoproj/config.yml
 ---
+apt_dpkg_update: true
 osdeps_mode: all
 GITORIOUS: ssh
 GITHUB: ssh
-apt_dpkg_update: true
-CODE_INTEGRATION: true
-CODE_MANAGE_FOLDERS: true
-CODE_ADD_CONFIG: true
 USE_PYTHON: true
-PYTHONPATH: /usr/bin/python2.7
+python_executable: "${PYTHON_EXECUTABLE}"
+GITHUB_ROOT: 'git@github.com:'
+GITHUB_PUSH_ROOT: 'git@github.com:'
+GITHUB_PRIVATE_ROOT: 'git@github.com:'
+GITORIOUS_ROOT: 'git@gitorious.org:'
+GITORIOUS_PUSH_ROOT: 'git@gitorious.org:'
+GITORIOUS_PRIVATE_ROOT: 'git@gitorious.org:'
+ros_version: 2
+ros_distro: 'foxy'
+user_shells:
+- bash
 EOF
 }
 
-export AUTOPROJ_OSDEPS_MODE=all
-export AUTOPROJ_BOOTSTRAP_IGNORE_NONEMPTY_DIR=1
-
-ruby autoproj_install
+ruby autoproj_install --gemfile=autoproj.gemfile
 . ./env.sh
-#autoproj bootstrap git ${BOOTSTRAP_URL}
+
+autoproj bootstrap git ${BOOTSTRAP_URL} branch=${BOOTSTRAP_BRANCH}
 
 aup
+
+amake --verbose
--- init.rb
+++ init.rb
@@ -1,39 +1,44 @@
+# Copyright 2021 Open Rise Robotics
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Write in this file customization code that will get executed before 
+# autoproj is loaded.
+
+# Set the path to 'make'
+# Autobuild.commands['make'] = '/path/to/ccmake'
+
+# Set the parallel build level (defaults to the number of CPUs)
+# Autobuild.parallel_build_level = 10
+
+# Uncomment to initialize the environment variables to default values. This is
+# useful to ensure that the build is completely self-contained, but leads to
+# miss external dependencies installed in non-standard locations.
+#
+# set_initial_env
+#
+# Additionally, you can set up your own custom environment with calls to env_add
+# and env_set:
+#
+# env_add 'PATH', "/path/to/my/tool"
+# env_set 'CMAKE_PREFIX_PATH', "/opt/boost;/opt/xerces"
+# env_set 'CMAKE_INSTALL_PATH', "/opt/orocos"
+#
+# NOTE: Variables set like this are exported in the generated 'env.sh' script.
+#
 require 'autoproj/gitorious'
+Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
+Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
 
 Autobuild.parallel_build_level = 10
 Autobuild::CMake.show_make_messages = true
-
-os_names, os_releases = Autoproj.workspace.operating_system
-
-ros_distro = ''
-so_distro=''
-
-distro_map = {
-    'xenial' => 'kinetic',
-    'bionic' => 'melodic',
-    'focal'  => 'noetic'
-}
-
-ros_distro = distro_map[os_releases.find { |release| distro_map[release]}]
-
-distro_map.each do |so_name,ros_name|
-  if (ros_distro == ros_name)
-    ros_distro= ros_name
-    so_distro=so_name
-  end
-end
-
-if ros_distro == 'noetic'
-  python_path = 'python3'
-else
-  python_path = 'python2.7'
-end
-
-Autoproj.message "  ros #{ros_distro}", :blue
-Autoproj.message "  distro #{so_distro}", :red
-Autoproj.message "  #{python_path} environment", :green
-
-Autoproj.env_add_path 'PYTHONPATH', File.join("python_path", 'dist-packages')
-Autoproj.config.set('USE_PYTHON', 'YES') unless Autoproj.config.get('USE_PYTHON', nil)
-
-Autobuild::CMake.prefix_path << Autoproj.prefix
